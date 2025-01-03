4cabf3e9c1ebbe919fef31a9d93d553189d1b0a7
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-01-03 18:48:01 +0530

    slider fixed

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import banner from '../../public/banner.png';
+import banner from '/banner.png';
 function Banner() {
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
         <div className=" order-2 md:order-1 w-full md:w-1/2 mt-12 md:mt-32">
 
         <div className="space-y-12">
