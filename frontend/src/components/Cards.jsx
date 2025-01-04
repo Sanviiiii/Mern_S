56ddbf300c0bb8071cd784c7526386fa8739e4ca
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-01-04 11:28:31 +0530

    cards added

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -8,7 +8,7 @@ function Cards({ item }) {
     <div className="card bg-base-100 w-96 shadow-xl">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src={item.image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
