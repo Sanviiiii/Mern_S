2ded2da74eabbf90f048dbd00cf30ba52640cd4f
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-02-18 21:11:29 +0530

    Map

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -46,13 +46,12 @@ function Signup() {
             <div className='flex justify-around mt-4'>
                     <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700'> Signup</button>
                     <p>Have Account? {" "} 
-                   <button className='underline text-blue-500 cursor-pointer'
+                  <button className='underline text-blue-500 cursor-pointer'
                              onClick={()=>document.getElementById("my_modal_3").showModal()} >
-
-                   Login
+                      Login
                    </button>  {" "}
                    <Login />
-                    </p> 
+                   </p> 
                 </div>
           </div>
         </div>
